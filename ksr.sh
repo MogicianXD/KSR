951006035abeda48159f069d3dbd852564a130e8
Mogician <1023276135@qq.com>
2020-03-16 15:53:41 +0800

    fix some bugs & remove kb and r from BP

--- ksr.sh
+++ ksr.sh
@@ -1,8 +1,8 @@
-nohup python -u run_KSR.py --data m15cat --type A --epochs 300 --lr 0.002 --cuda 1 --pretrain > m15A2 2>&1 &
-nohup python -u run_KSR.py --data m15cat --type B --epochs 300 --lr 0.002 --cuda 3 --pretrain > m15B2 2>&1 &
+nohup python -u run_KSR.py --data m15cat --type A --epochs 300 --lr 0.002 --cuda 3 --pretrain > m15A0 2>&1 &
+nohup python -u run_KSR.py --data m15cat --type B --epochs 300 --lr 0.002 --cuda 5 --pretrain > m15B0 2>&1 &
 
-#nohup python -u run_KSR.py --data m15cat --type A --epochs 300 --lr 0.002 --cuda 3 > m15A 2>&1 &
-#nohup python -u run_KSR.py --data m15cat --type B --epochs 300 --lr 0.002 --cuda 2 > m15B 2>&1 &
+#nohup python -u run_KSR.py --data m15cat --type A --epochs 300 --lr 0.002 --cuda 4 > m15A3 2>&1 &
+#nohup python -u run_KSR.py --data m15cat --type B --epochs 300 --lr 0.002 --cuda 5 > m15B3 2>&1 &
 
 #nohup python -u run_KSR.py --data year15 --type A --epochs 300 --lr 0.002 --cuda 2 --pretrain > y15A2 2>&1 &
 #nohup python -u run_KSR.py --data year15 --type A --epochs 300 --lr 0.002 --cuda 4 > y15A 2>&1 &
